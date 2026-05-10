b1b1b70d5e58161a55a179ddfa3c20746f12aeca
vova <vova.davaev01@mail.ru>
2026-05-10 12:35:45 +0300

    Попытка 1

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,31 +1,143 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  NotFound404,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
-
-import { AppHeader } from '@components';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import { AppHeader, IngredientDetails, OrderInfo } from '@components';
 import { Preloader } from '@ui';
+import {
+  ProtectedRoute,
+  UnAuthRoute
+} from '../protected-route/protected-route';
+import { Modal } from '@components';
+import { basename } from 'path';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from '../../services/store';
+import { TIngredient } from '@utils-types';
+import {
+  selectIngredients,
+  getIngredientsThunk
+} from '../../services/slices/ingredients/ingredients-slice';
+import {
+  checkUserAuth,
+  setIsAuthChecked
+} from '../../services/slices/user/actions';
 
 const App = () => {
-  /** TODO: взять переменные из стора */
-  const isIngredientsLoading = false;
-  const ingredients = [];
-  const error = null;
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const ingredients: TIngredient[] = useSelector(selectIngredients);
 
+  const backgroundLocation = location.state?.background;
+  const onCloseModal = () => {
+    navigate(-1);
+  };
+
+  useEffect(() => {
+    dispatch(checkUserAuth()).finally(() => dispatch(setIsAuthChecked(true)));
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (!ingredients.length) {
+      dispatch(getIngredientsThunk());
+    }
+  }, [dispatch, ingredients.length]);
   return (
     <div className={styles.app}>
       <AppHeader />
-      {isIngredientsLoading ? (
-        <Preloader />
-      ) : error ? (
-        <div className={`${styles.error} text text_type_main-medium pt-4`}>
-          {error}
-        </div>
-      ) : ingredients.length > 0 ? (
-        <ConstructorPage />
-      ) : (
-        <div className={`${styles.title} text text_type_main-medium pt-4`}>
-          Нет игредиентов
-        </div>
+      <Routes location={backgroundLocation || location}>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route
+          path='/login'
+          element={
+            <UnAuthRoute>
+              <Login />
+            </UnAuthRoute>
+          }
+        />
+        <Route
+          path='/register'
+          element={
+            <UnAuthRoute>
+              <Register />
+            </UnAuthRoute>
+          }
+        />
+        <Route
+          path='/forgot-password'
+          element={
+            <UnAuthRoute>
+              <ForgotPassword />
+            </UnAuthRoute>
+          }
+        />
+        <Route
+          path='/reset-password'
+          element={
+            <UnAuthRoute>
+              <ResetPassword />
+            </UnAuthRoute>
+          }
+        />
+        <Route
+          path='/profile'
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/profile/orders'
+          element={
+            <ProtectedRoute>
+              <ProfileOrders />
+            </ProtectedRoute>
+          }
+        />
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+
+      {backgroundLocation && (
+        <Routes>
+          <Route
+            path='feed/:number'
+            element={
+              <Modal title={''} onClose={onCloseModal}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='ingredients/:id'
+            element={
+              <Modal title={'Детали ингредиента'} onClose={onCloseModal}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <ProtectedRoute>
+                <Modal title={''} onClose={onCloseModal}>
+                  <OrderInfo />
+                </Modal>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
       )}
     </div>
   );
--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -2,23 +2,70 @@ import { FC, useMemo } from 'react';
 import { TConstructorIngredient } from '@utils-types';
 import { BurgerConstructorUI } from '@ui';
 
+import { useSelector, useDispatch } from '../../services/store';
+import {
+  selectNewOrder,
+  selectOrderRequest,
+  setNewOrder
+} from '../../services/slices/orders/orders-slice';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { postUserBurgerThunk } from '../../services/slices/orders/actions';
+import {
+  clearBurger,
+  selectBurgerConstructor
+} from '../../services/slices/constructor/constructor-slice';
+import { selectUser } from '../../services/slices/user/user-slice';
+
 export const BurgerConstructor: FC = () => {
-  /** TODO: взять переменные constructorItems, orderRequest и orderModalData из стора */
-  const constructorItems = {
-    bun: {
-      price: 0
-    },
-    ingredients: []
-  };
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const user = useSelector(selectUser);
 
-  const orderRequest = false;
+  const constructorItems = useSelector(selectBurgerConstructor);
 
-  const orderModalData = null;
+  const orderRequest = useSelector(selectOrderRequest);
+
+  const orderModalData = useSelector(selectNewOrder).order;
 
   const onOrderClick = () => {
-    if (!constructorItems.bun || orderRequest) return;
+    if (!constructorItems.bun || orderRequest) {
+      return;
+    }
+
+    if (!user) {
+      return navigate('/login', {
+        replace: true,
+        state: {
+          from: {
+            ...location,
+            background: location.state?.background,
+            state: null
+          }
+        }
+      });
+    } else {
+      const from = location.state?.from || { pathname: '/' };
+      const backgroundLocation = location.state?.from?.background || null;
+
+      const itemsId = [
+        constructorItems.bun._id,
+        ...constructorItems.ingredients.map((ingredient) => ingredient._id),
+        constructorItems.bun._id
+      ];
+
+      dispatch(postUserBurgerThunk(itemsId)).then(() =>
+        dispatch(clearBurger())
+      );
+      return navigate(from, {
+        replace: true,
+        state: { background: backgroundLocation }
+      });
+    }
   };
-  const closeOrderModal = () => {};
+  const closeOrderModal = () => {
+    dispatch(setNewOrder(false));
+  };
 
   const price = useMemo(
     () =>
@@ -29,8 +76,6 @@ export const BurgerConstructor: FC = () => {
       ),
     [constructorItems]
   );
-
-  return null;
 
   return (
     <BurgerConstructorUI
